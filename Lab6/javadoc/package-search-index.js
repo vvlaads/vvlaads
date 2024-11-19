66be9e2d89363d5e7a98d5ca0fa3d3459abcdf69
vvlaads <114498556+vvlaads@users.noreply.github.com>
2024-11-19 12:41:14 +0300

    Delete Lab6 directory

--- Lab6/javadoc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"client"},{"l":"server"},{"l":"utility"},{"l":"utility.calculate"},{"l":"utility.command"},{"l":"utility.comparators"},{"l":"utility.manager"},{"l":"utility.parser"},{"l":"utility.worker"}];updateSearchResults();
