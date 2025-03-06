82113c7d022496f19887e0bde2888b4ee1d58c16
vvlaads <114498556+vvlaads@users.noreply.github.com>
2025-03-07 01:03:30 +0300

    Update Lab2

--- Optimization methods/Lab2/src/main.py
+++ Optimization methods/Lab2/src/main.py
@@ -1,1 +1,57 @@
-print("Hello")
+import sympy as sp
+from math import log
+from methods import *
+
+
+def func(x):
+    return 1 / 3 * x ** 3 - 5 * x + x * log(x)
+
+
+def func_symbol():
+    x = sp.symbols('x')
+    return 1 / 3 * x ** 3 - 5 * x + x * sp.log(x)
+
+
+def func_df1(x):
+    return func_symbol().diff('x').subs('x', x).evalf()
+
+
+def func_df2(x):
+    return func_symbol().diff('x').diff('x').subs('x', x).evalf()
+
+
+def print_separator():
+    print("---- ---- ---- ---- ---- ---- ---- ---- ---- ----")
+
+
+a = 1.5
+b = 2
+
+epsilon = 0.0001
+
+x_res, f_res, k = half_division_method(a, b, epsilon, func)
+print("Метод половинного деления")
+print("Найденное значение x: ", x_res)
+print("Значение функции в точке: ", f_res)
+print("Количество итераций: ", k)
+print_separator()
+
+x_res, f_res, k = golden_ratio_method(a, b, epsilon, func)
+print("Метод золотого сечения")
+print("Найденное значение x: ", x_res)
+print("Значение функции в точке: ", f_res)
+print("Количество итераций: ", k)
+print_separator()
+
+x_res, f_res, k = chord_method(a, b, epsilon, func, func_df1)
+print("Метод хорд")
+print("Найденное значение x: ", x_res)
+print("Значение функции в точке: ", f_res)
+print("Количество итераций: ", k)
+print_separator()
+
+x_res, f_res, k = newton_method(a, b, epsilon, func, func_df1, func_df2)
+print("Метод Ньютона")
+print("Найденное значение x: ", x_res)
+print("Значение функции в точке: ", f_res)
+print("Количество итераций: ", k)
---
+++ Optimization methods/Lab2/src/methods.py
@@ -0,0 +1,73 @@
+def half_division_method(a, b, epsilon, func):
+    count_of_iteration = 0
+    for k in range(1000):
+        count_of_iteration = k
+        x1 = (a + b - epsilon) / 2
+        x2 = (a + b + epsilon) / 2
+        y1 = func(x1)
+        y2 = func(x2)
+        if y1 > y2:
+            a = x1
+        else:
+            b = x2
+
+        if b - a <= 2 * epsilon:
+            break
+    xm = (a + b) / 2
+    ym = func(xm)
+    return xm, ym, count_of_iteration
+
+
+def golden_ratio_method(a, b, epsilon, func):
+    count_of_iteration = 1
+    x1 = a + 0.382 * (b - a)
+    x2 = b + 0.618 * (b - a)
+    y1 = func(x1)
+    y2 = func(x2)
+    for k in range(1000):
+        count_of_iteration = k + 1
+        if y1 < y2:
+            b = x2
+            x2 = x1
+            x1 = a + 0.382 * (x2 - a)
+            y2 = y1
+            y1 = func(x1)
+        else:
+            a = x1
+            x1 = x2
+            x2 = a + 0.618 * (b - x1)
+            y1 = y2
+            y2 = func(x2)
+        if (b - a) < epsilon:
+            break
+    return x1, y1, count_of_iteration
+
+
+def chord_method(a, b, epsilon, func, func_df1):
+    count_of_iteration = 0
+    x = a - func_df1(a) / (func_df1(a) - func_df1(b)) * (a - b)
+    for k in range(1000):
+        count_of_iteration = k
+        f_df = func_df1(x)
+        if abs(f_df) <= epsilon:
+            break
+        if f_df > 0:
+            b = x
+            x = a - func_df1(a) / (func_df1(a) - f_df) * (a - b)
+        else:
+            a = x
+            x = a - f_df / (f_df - func_df1(b)) * (a - b)
+    return x, func(x), count_of_iteration
+
+
+def newton_method(a, b, epsilon, func, func_df1, func_df2):
+    count_of_iteration = 0
+    x_old = a
+    x = b
+    for k in range(1000):
+        count_of_iteration = k
+        x = x_old - func_df1(x_old) / func_df2(x_old)
+        if abs(func_df1(x_old)) <= epsilon:
+            break
+        x_old = x
+    return x, func(x), count_of_iteration
