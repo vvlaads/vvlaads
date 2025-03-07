24139dabf804eb66be1d045a34ebc90ae3244ddc
vvlaads <114498556+vvlaads@users.noreply.github.com>
2025-03-07 10:53:01 +0300

    Added Lab3

--- Optimization methods/Lab3/src/main.py
+++ Optimization methods/Lab3/src/main.py
@@ -1,1 +1,18 @@
+from math import log
+from method import *
 
+
+def func(x):
+    return 1 / 3 * x ** 3 - 5 * x + x * log(x)
+
+
+x1 = 1.5
+delta_x = 1
+epsilon1 = 0.0001
+epsilon2 = 0.0001
+
+x_res, f_res, k = method(x1, delta_x, epsilon1, epsilon2, func)
+print("Метод квадратичной аппроксимации")
+print("Значение x: ", x_res)
+print("Значение функции в точке: ", f_res)
+print("Количество итераций: ", k)
---
+++ Optimization methods/Lab3/src/method.py
@@ -0,0 +1,61 @@
+def method(x1, delta_x, epsilon1, epsilon2, func):
+    count_of_iteration = 0
+    f1, f2, f3 = 0, 0, 0
+    x2, x3 = 0, 0
+    x_average, f_average = 0, 0
+    skip = False
+    for k in range(1, 1000):
+        count_of_iteration = k
+        if not skip:
+            # Шаг 2
+            x2 = x1 + delta_x
+
+            # Шаг 3
+            f1 = func(x1)
+            f2 = func(x2)
+
+            # Шаг 4
+            if f1 > f2:
+                x3 = x1 + 2 * delta_x
+            else:
+                x3 = x1 - delta_x
+
+            # Шаг 5
+            f3 = func(x3)
+
+        # Шаг 6
+        f_min = min(f1, f2, f3)
+        if f_min == f1:
+            x_min = x1
+        elif f_min == f2:
+            x_min = x2
+        else:
+            x_min = x3
+
+        # Шаг 7
+        if (x2 - x1) * f1 + (x3 - x1) * f2 + (x1 - x2) * f3 == 0:
+            x1 = x_min
+            skip = False  # Перейти к шагу 2
+            continue
+
+        x_average = 1 / 2 * ((x2 ** 2 - x3 ** 2) * f1 + (x3 ** 2 - x1 ** 2) * f2 + (x1 ** 2 - x2 ** 2) * f3) / (
+                (x2 - x3) * f1 + (x3 - x1) * f2 + (x1 - x2) * f3)
+        f_average = func(x_average)
+
+        # Шаг 8
+        if abs((f_min - f_average) / f_average) < epsilon1 and abs((x_min - x_average) / x_average) < epsilon2:
+            return x_average, f_average, count_of_iteration  # Конец
+        elif x1 <= x_average <= x3:
+            if f_min < f_average:
+                x2 = x_min
+            else:
+                x2 = x_average
+            f2 = func(x2)
+            skip = True  # Перейти к шагу 6
+            continue
+        else:
+            x1 = x_average
+            skip = False  # Перейти к шагу 2
+            continue
+
+    return x_average, f_average, count_of_iteration
