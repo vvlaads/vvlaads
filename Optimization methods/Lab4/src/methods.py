7aeb2240715b8fdf9bf6e2178cff31240b06ce04
vvlaads <114498556+vvlaads@users.noreply.github.com>
2025-04-11 10:11:11 +0300

    Update Lab4

--- Optimization methods/Lab4/src/methods.py
+++ Optimization methods/Lab4/src/methods.py
@@ -1,6 +1,7 @@
 import math
 
 
+# Метод половинного деления для поиска минимума в одномерном случае
 def half_division_method(a, b, epsilon, func):
     for k in range(1, 1000):
         x1 = (a + b - epsilon) / 2
@@ -12,91 +13,104 @@ def half_division_method(a, b, epsilon, func):
 
         if b - a <= 2 * epsilon:
             break
-    xm = (a + b) / 2
-    return xm
+    return (a + b) / 2
 
 
+# Метод покоординатного спуска
 def coordinate_descent(func, x0, y0, epsilon):
     count_of_iteration = 0
-    vector = [0, 0]
     result = 0
     x_cur = x0
     y_cur = y0
 
     for n in range(1, 1000):
-        def f1(x):
-            return func(x, y_cur)
-
-        def f2(y):
-            return func(x_cur, y)
-
         count_of_iteration = n
+
+        # Точка на k-ой итерации
         x_start = x_cur
         y_start = y_cur
 
-        a = x_cur - 1
-        b = x_cur + 1
+        # Фиксируем значение y
+        def f1(x):
+            return func(x, y_cur)
+
+        # Находим минимум по x
+        a = x_cur - 5
+        b = x_cur + 5
         x_cur = half_division_method(a, b, epsilon, f1)
 
-        a = y_cur - 1
-        b = y_cur + 1
+        # Фиксируем значение x
+        def f2(y):
+            return func(x_cur, y)
+
+        # Находим минимум по y
+        a = y_cur - 5
+        b = y_cur + 5
         y_cur = half_division_method(a, b, epsilon, f2)
 
+        # Сравниваем значения функций в точках на k-ой итерации и на (k+1)-ой итерации
         if abs(func(x_cur, y_cur) - func(x_start, y_start)) <= epsilon:
             result = func(x_cur, y_cur)
-            vector[0] = x_cur
-            vector[1] = y_cur
             break
 
-    return result, vector, count_of_iteration
+    return result, x_cur, y_cur, count_of_iteration
 
 
+# Метод градиентного спуска
 def gradient_descent(func, x0, y0, epsilon, f_dx, f_dy):
-    alpha = 1
-    result = 0
-    vector = [0, 0]
     count_of_iteration = 0
+    result = 0
+    alpha = 0.1  # Размер шага
     x_cur = x0
     y_cur = y0
     for n in range(1, 1000):
         count_of_iteration = n
-        x1 = x_cur - alpha * f_dx(x_cur, y_cur)
-        y1 = y_cur - alpha * f_dy(x_cur, y_cur)
-        if func(x1, y1) > func(x_cur, y_cur):
+
+        # Точка на k-ой итерации
+        x_start = x_cur
+        y_start = y_cur
+
+        # Точка на (k+1)-ой итерации
+        x_cur = x_start - alpha * f_dx(x_start, y_start)
+        y_cur = y_start - alpha * f_dy(x_start, y_start)
+
+        # Уменьшаем шаг, если значение функции больше предыдущего
+        if func(x_cur, y_cur) > func(x_start, y_start):
             alpha /= 10
-        if abs(func(x1, y1) - func(x_cur, y_cur)) < epsilon:
-            vector[0] = x1
-            vector[1] = y1
-            result = func(x1, y1)
-            break
-        x_cur = x1
-        y_cur = y1
 
-    return result, vector, count_of_iteration
-
-
-def fastest_descent(func, x0, y0, epsilon, f_dx, f_dy):
-    x_cur = x0
-    y_cur = y0
-    result = 0
-    vector = [0, 0]
-    count_of_iteration = 0
-    for n in range(1, 1000):
-        count_of_iteration = n
-        f1 = f_dx(x_cur, y_cur)
-        f2 = f_dy(x_cur, y_cur)
-        norm = math.sqrt(f1 ** 2 + f2 ** 2)
-        if norm < epsilon:
-            vector[0] = x_cur
-            vector[1] = y_cur
+        # Сравниваем значения функций в точках на k-ой итерации и на (k+1)-ой итерации
+        if abs(func(x_cur, y_cur) - func(x_start, y_start)) < epsilon:
             result = func(x_cur, y_cur)
             break
 
-        def alpha_func(value):
+    return result, x_cur, y_cur, count_of_iteration
+
+
+# Метод наискорейшего спуска
+def fastest_descent(func, x0, y0, epsilon, f_dx, f_dy):
+    count_of_iteration = 0
+    result = 0
+    x_cur = x0
+    y_cur = y0
+    for n in range(1, 1000):
+        count_of_iteration = n
+
+        # Находим частные производные
+        f1 = f_dx(x_cur, y_cur)
+        f2 = f_dy(x_cur, y_cur)
+
+        # Выходим, если норма градиента достаточно мала
+        norm = math.sqrt(f1 ** 2 + f2 ** 2)
+        if norm < epsilon:
+            result = func(x_cur, y_cur)
+            break
+
+        # Находим оптимальный шаг
+        def h_func(value):
             return func(x_cur - value * f1, y_cur - value * f2)
 
-        alpha = half_division_method(-100, 100, epsilon, alpha_func)
+        step = half_division_method(-10, 10, epsilon, h_func)
 
-        x_cur -= alpha * f1
-        y_cur -= alpha * f2
-    return result, vector, count_of_iteration
+        x_cur -= step * f1
+        y_cur -= step * f2
+    return result, x_cur, y_cur, count_of_iteration
